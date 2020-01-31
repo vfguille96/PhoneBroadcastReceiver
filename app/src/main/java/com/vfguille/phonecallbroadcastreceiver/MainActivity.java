78093014e8c38db4b27748b4578fb4c24cd0a082
Guillermo Vera <vfguille@gmail.com>
2020-01-31 10:34:16 +0100

    Permisos

--- app/src/main/java/com/vfguille/phonecallbroadcastreceiver/MainActivity.java
+++ app/src/main/java/com/vfguille/phonecallbroadcastreceiver/MainActivity.java
@@ -1,14 +1,40 @@
 package com.vfguille.phonecallbroadcastreceiver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int PERMISSION_REQUEST_PHONE_STATE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        requestPhonePermission();
+    }
+
+    private void requestPhonePermission() {
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_REQUEST_PHONE_STATE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults.length > 0 && requestCode == PERMISSION_REQUEST_PHONE_STATE) {
+            if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "No has aceptado los permisos, wey", Toast.LENGTH_LONG).show();
+        }
     }
 }
